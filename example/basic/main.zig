c0330a4057eee00e03c52db7f43b35385ae7cbaa
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-07-30 19:15:20 +0900

    fix example

--- example/basic/main.zig
+++ example/basic/main.zig
@@ -3,13 +3,24 @@ const curl = @import("curl");
 
 pub fn main() anyerror!void {
     var allocator = std.heap.page_allocator;
+
+    var cainfo = try std.process.getEnvVarOwned(allocator, "CURL_CA_BUNDLE");
+    defer allocator.free(cainfo);
+
     var f = struct {
-        fn f(_: curl.response, resp: []const u8) anyerror!usize {
-            try std.io.getStdOut().writeAll(resp);
-            return resp.len;
+        fn f(data: []const u8) anyerror!usize {
+            try std.io.getStdOut().writeAll(data);
+            return data.len;
         }
     }.f;
-    var res = try curl.get("https://google.com/", .{ .allocator = allocator, .cb = f });
+
+    var req = curl.request{
+        .allocator = allocator,
+        .cb = f,
+        .sslVerify = true,
+        .cainfo = cainfo,
+    };
+    var res = try curl.get("https://google.com/", req);
     if (res != 0) {
         var msg = try curl.strerrorAlloc(allocator, res);
         defer allocator.free(msg);
